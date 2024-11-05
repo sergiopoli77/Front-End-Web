be355683e1d9fdd6fa3b69806ef8df13a7ce6862
sergiopoli77 <S22210048@student.unklab.ac.id>
2024-11-05 08:08:36 +0800

    axios - useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Hello from "./Components/Hello";
 import World from "./Components/World";
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import axios from "axios";
 
 const App = () => {
   //let hello = "Selamat Datang";
-  const [hello, setHello] = useState("Selamat Datang");
+  const [hello, setHello] = useState("Selamat Datang")
+  
+
+  useEffect (() => {
+    console.log('component did mount')
+  }, []);
 
   const handleClick = () => {
     // hello = "Welcome";
