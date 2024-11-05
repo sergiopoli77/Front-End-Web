f855d9c489cc108687d663a746db643401cc01b2
sergiopoli77 <S22210048@student.unklab.ac.id>
2024-11-05 08:02:50 +0800

    adding onClick - useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Hello from './Components/Hello'
-import World from './Components/World'
+import Hello from "./Components/Hello";
+import World from "./Components/World";
+import { useState } from "react";
 
-const App = () =>{
-  const hello = "Hello World";
+const App = () => {
+  //let hello = "Selamat Datang";
+  const [hello, setHello] = useState("Selamat Datang");
+
+  const handleClick = () => {
+    // hello = "Welcome";
+    setHello("Welcome");
+    console.log("hello");
+  };
+
   return (
-    <div>
+    <>
       <h1>{hello}</h1>
       <h2> Hello Wolrd 1</h2>
-      <Hello text = "Component Hello 1" number={100}/>
-      <Hello text = "Component Hello 2" number={110}/>
+      <Hello text="Component Hello 1" number={100} />
+      <Hello text="Component Hello 2" number={110} />
       <World />
-    </div>
-  )
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
 };
 
-
-
-
-
-export default App
+export default App;
--- src/Components/Hello/index.js
+++ src/Components/Hello/index.js
@@ -1,8 +1,10 @@
-const Hello = ({text}) => {
-    return (
-        <h3>{text}</h3>
-        
-    )
-  }
-  
-  export default Hello;
+const Hello = ({ text, number }) => {
+  return (
+    <>
+      <h3>{text}</h3>
+      <h3>{number}</h3>
+    </>
+  );
+};
+
+export default Hello;
